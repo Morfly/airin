3a59e54305a0a14e85d4894302e3e8b18aa17d7c
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-11-08 20:14:23 -0800

    added dependency collection typealias

--- airin-core/src/jvmMain/kotlin/io/morfly/airin/AddedDependencyHolder.kt
+++ airin-core/src/jvmMain/kotlin/io/morfly/airin/AddedDependencyHolder.kt
@@ -2,9 +2,11 @@ package io.morfly.airin
 
 import io.morfly.airin.label.Label
 
+typealias AddedDependencyCollection = MutableMap<ConfigurationName, MutableSet<Label>>
+
 interface AddedDependencyHolder {
 
-    val addedDependencies: MutableMap<ConfigurationName, MutableSet<Label>>
+    val addedDependencies: AddedDependencyCollection
 
     fun addDependency(label: Label, configuration: String) {
         addedDependencies.getOrPut(configuration, ::mutableSetOf) += label
--- airin-core/src/jvmMain/kotlin/io/morfly/airin/FeatureContext.kt
+++ airin-core/src/jvmMain/kotlin/io/morfly/airin/FeatureContext.kt
@@ -1,6 +1,5 @@
 package io.morfly.airin
 
-import io.morfly.airin.label.Label
 import io.morfly.pendant.starlark.lang.ModifierCollection
 import io.morfly.pendant.starlark.lang.ModifiersHolder
 
@@ -15,5 +14,5 @@ class FeatureContext(
 
     override val configurationOverrides: ConfigurationOverrideCollection = mutableMapOf()
 
-    override val addedDependencies = mutableMapOf<ConfigurationName, MutableSet<Label>>()
+    override val addedDependencies: AddedDependencyCollection = mutableMapOf()
 }
