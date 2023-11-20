b945583316acb1ee73b9ea3916abc4931f5e53a6
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-11-19 22:24:08 -0800

    removed old code

--- airin-gradle-plugin/src/main/kotlin/io/morfly/airin/StarlarkExtensions.kt
+++ airin-gradle-plugin/src/main/kotlin/io/morfly/airin/StarlarkExtensions.kt
@@ -4,8 +4,8 @@ import io.morfly.airin.label.MavenCoordinates
 import io.morfly.pendant.starlark.artifact
 import io.morfly.pendant.starlark.lang.context.FunctionCallContext
 
-fun FunctionCallContext.applyDependenciesFrom(packageDescriptor: GradleModule) {
-    for ((configuration, dependencies) in packageDescriptor.dependencies) {
+fun FunctionCallContext.applyDependenciesFrom(module: GradleModule) {
+    for ((configuration, dependencies) in module.dependencies) {
         configuration `=` dependencies.mapNotNull {
             when (it) {
                 is MavenCoordinates -> artifact(it.asShortLabel().toString())
