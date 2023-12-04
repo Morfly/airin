03515b80b8e72fb860b251232e23e6088e79b72f
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-12-03 21:41:21 -0800

    add publishing to gradle plugins portal

--- airin-gradle-android/build.gradle.kts
+++ airin-gradle-android/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `kotlin-dsl`
     alias(libs.plugins.airin.maven.publish)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.gradle.publish)
 }
 
 kotlin {
@@ -19,10 +20,25 @@ dependencies {
 }
 
 gradlePlugin {
+    website = "https://github.com/Morfly/airin"
+    vcsUrl = "https://github.com/Morfly/airin"
+
     plugins {
         val airin by registering {
             id = "io.morfly.airin.android"
+            displayName = "airin"
+            description = "A plugin for automated the migration from Gradle to Bazel."
+            tags = listOf("android", "bazel", "kotlin", "starlark", "migration")
             implementationClass = "io.morfly.airin.plugin.AirinAndroidGradlePlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "localPluginRepository"
+            url = uri("../local-plugin-repository")
         }
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.vanniktech.maven.publish) apply false
     alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.gradle.publish) apply false
 }
