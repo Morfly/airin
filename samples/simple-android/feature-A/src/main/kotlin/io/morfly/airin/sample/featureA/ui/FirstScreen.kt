1ac83a5a1ca8c98c200b04e974717773963dbd5d
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-12-13 20:45:11 -0800

    improve UI of first screen in a sample project

--- samples/simple-android/feature-A/src/main/kotlin/io/morfly/airin/sample/featureA/ui/FirstScreen.kt
+++ samples/simple-android/feature-A/src/main/kotlin/io/morfly/airin/sample/featureA/ui/FirstScreen.kt
@@ -1,9 +1,20 @@
 package io.morfly.airin.sample.featureA.ui
 
-import androidx.compose.foundation.clickable
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -12,8 +23,33 @@ fun FirstScreen(
     onNextClick: () -> Unit,
     viewModel: FirstViewModel = hiltViewModel()
 ) {
-    Text(
-        text = "FirstScreen",
-        Modifier.clickable { onNextClick() }
-    )
+    Box(
+        modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "First Screen",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = onNextClick) {
+                Text(
+                    text = "Open Second Screen",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun FirstScreenPreview() {
+    FirstScreen(onNextClick = {})
 }
--- samples/simple-android/feature-A/src/main/kotlin/io/morfly/airin/sample/featureA/ui/FirstViewModel.kt
+++ samples/simple-android/feature-A/src/main/kotlin/io/morfly/airin/sample/featureA/ui/FirstViewModel.kt
@@ -5,5 +5,4 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class FirstViewModel @Inject constructor(): ViewModel() {
-}
+class FirstViewModel @Inject constructor() : ViewModel()
